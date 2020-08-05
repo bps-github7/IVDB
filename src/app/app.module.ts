6f3cde9c433698b3e32e86455bc988b5f0a1d465
Ben Sehnert <bps53@protonmail.com>
2020-08-05 17:35:00 -0400

    added a working dropdown menu and routing. many components for the site subtree were added. began making forms (sign in. create acct), not a lot of headway there.

---
+++ src/app/app.module.ts
@@ -0,0 +1,100 @@
+//imports and decorators
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+//declarations/ bootstraps
+import { AppComponent } from './app.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { PanelComponent } from './panel/panel.component';
+import { ZippyComponent } from './zippy/zippy.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ForumComponent } from './forum/forum.component';
+import { GamesComponent } from './games/games.component';
+import { StreamingComponent } from './streaming/streaming.component';
+import { ReccomendationsComponent } from './reccomendations/reccomendations.component';
+import { WatchlistsComponent } from './watchlists/watchlists.component';
+import { RateComponent } from './rate/rate.component';
+import { SearchComponent } from './search/search.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DropdownComponent,
+    FavoriteComponent,
+    PanelComponent,
+    ZippyComponent,
+    HomeComponent,
+    SignInComponent,
+    CreateAccountComponent,
+    ProfileComponent,
+    ForumComponent,
+    GamesComponent,
+    StreamingComponent,
+    ReccomendationsComponent,
+    WatchlistsComponent,
+    RateComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+        {
+            path: '', component: HomeComponent
+        },
+        {
+            path: 'sign_in/profile/:username', component: ProfileComponent
+        },
+        {
+            path: 'sign_in/createAccount', component: CreateAccountComponent
+        },
+        {
+            path: 'sign_in', component: SignInComponent
+        },
+        {
+            path: 'forum', component: ForumComponent
+        },
+        {
+            path: 'games', component: GamesComponent
+        },
+        {
+            path: 'streaming', component: StreamingComponent
+        },
+        {
+            path: 'reccomendations', component: ReccomendationsComponent
+        },
+        {
+            path: 'watchlists', component: WatchlistsComponent
+        },
+        {
+            path: 'rate', component: RateComponent
+        },
+        {
+            path: 'search', component: SearchComponent
+        },
+        //uncomment this aftr you create NotFoundComponent- PWM code is outdated, for older angular version, and does not work.
+        // {
+        //     path: '**', component: NotFoundComponent
+        // }
+    ])
+  ],
+  providers: [
+      /*Some ideas:
+        ~watchlists.service
+        ~Reccomendations.service
+        ~sign-in.authenticator.service
+        ~forum.service
+      */
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
