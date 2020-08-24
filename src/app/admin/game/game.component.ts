53c033e665caa8063cae71ddef400aa1a376749d
Ben Sehnert <bps53@protonmail.com>
2020-08-24 16:11:58 -0400

    Reverted changes to remove and reimplement search (filter method of Game.component.ts)
    this is stable. everything is working. branch from here
    if the re implementation breaks the table.

--- src/app/admin/game/game.component.ts
+++ src/app/admin/game/game.component.ts
@@ -11,28 +11,29 @@ export class GameComponent implements OnInit, OnDestroy {
     // trying this out for size
     // games: Game;
     games: any[];
+    games$;
     filteredGames: any[];
     subscription: Subscription;
 
     constructor(private gameService : GameService) {
-        this.subscription = gameService.getAllModified()
-            .subscribe(games => this.filteredGames = this.games = games);    
+        this.games$ = this.gameService.getAll()
+        // this.subscription = gameService.getAllModified()
+        //     .subscribe(games => this.filteredGames = this.games = games);    
     }
 
-    filter(query: string) {
+    filter(query: HTMLInputElement) {
         //when the site is up and running
         /// we will want to perform this query on the server.
         /// major efficiency drain otherwise!!!
-        this.filteredGames = (query) ?
-            this.games.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
-            this.games;
-        // console.log(query);
+        // this.filteredGames = (query) ?
+        //     this.games$.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+        //     this.games;
+        console.log(query.value);
     }
 
     ngOnDestroy(): void {
-        this.subscription.unsubscribe();
-
-    }
+        // this.subscription.unsubscribe();
+        console.log("does a thing!");    }
 
     ngOnInit(): void {
     }
