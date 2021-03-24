610093ad4577456e67ec7f93d6812ad368b7be2f
Ben Sehnert <bps53@protonmail.com>
2021-03-24 14:22:36 -0400

    Finished up ContributionsRoutingModule.ts's ContributionsComponents.
    Maybe a little overkill. We still need to
    split modules up into the helper components and the SPs.

--- src/app/modules/contributions/contributions.routing.ts
+++ src/app/modules/contributions/contributions.routing.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core'; 
 import { Routes, RouterModule } from '@angular/router'; 
+import { AuthGuard } from 'src/app/common/services/auth-guard.service';
+import { ContribDashboardComponent } from './contrib-dashboard/contrib-dashboard.component';
+import { ContributionsComponent } from './contributions/contributions.component';
+import { DisplayAverageRatingComponent } from './display-average-rating/display-average-rating.component';
+import { DisplayRatingComponent } from './display-rating/display-rating.component';
+import { DisplayReviewComponent } from './display-review/display-review.component';
+import { EditReviewComponent } from './edit-review/edit-review.component';
+import { SuggestNewComponent } from './suggest-new/suggest-new.component';
+import { ViewPostsComponent } from './view-posts/view-posts.component';
+import { ViewRatingsComponent } from './view-ratings/view-ratings.component';
+import { ViewReviewsComponent } from './view-reviews/view-reviews.component';
 
 const routes: Routes = [ 
     { path : ':username/reviews/new', component:  EditReviewComponent, canActivate: [AuthGuard]},
     { path : ':username/reviews/edit', component: EditReviewComponent, canActivate: [AuthGuard]},
     { path : ':username/contributions', component:  ContributionsComponent, canActivate: [AuthGuard]},
+        //TODO: finish routes above.
 
 ];
 @NgModule({ 
@@ -16,7 +28,14 @@ no need exposing/ making things public  */
 export class ContributionsRoutingModule { } 
     export const ContributionsComponents = [
         ContribDashboardComponent,
-        ContributionsComponents,
-        //TODO: finish this contribComponents array and routes above.
+        ContributionsComponent,
+        DisplayAverageRatingComponent,
+        DisplayRatingComponent,
+        DisplayReviewComponent,
+        EditReviewComponent,
+        SuggestNewComponent,
+        ViewPostsComponent,
+        ViewRatingsComponent,
+        ViewReviewsComponent
 
     ]
