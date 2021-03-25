6e05b8131d6003660316fc1fc65f7a820f5c08b9
Ben Sehnert <bps53@protonmail.com>
2021-03-25 12:29:10 -0400

    After near disaster struck, we volleyed between using a new project and copy and pasting from the old one, project diretory and vscode workspace got messy, completely failed at using git for its intended purpose. real learning experience id say.. now things are missing from node modules and our lock file, so we are running ng update, npm install x, y, until this point where ive been asked to stache changes, clean the repository and try runnning the commands again. Until we are done here, some very important stuff is missing like 'NgModule', 'Routes', 'RouterModule' things from @angular/core. Not really sure how this happened but it is not at all suprising given the absolute maelstrom that was last nights turn of events.

--- src/app/modules/admin/admin.routing.ts
+++ src/app/modules/admin/admin.routing.ts
@@ -2,34 +2,34 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; 
 import { AdminAuthGuard } from 'src/app/common/services/admin-auth-guard.service';
 import { AuthGuard } from 'src/app/common/services/auth-guard.service';
-import { ConsoleFormComponent } from './forms/console-form/console-form.component';
-import { ConsoleInfoFormComponent } from './forms/console-info-form/console-info-form.component';
-import { ConsolePostComponent } from './forms/console-post/console-post.component';
-import { DescriptorFormComponent } from './forms/descriptor-form/descriptor-form.component';
-import { GameFormComponent } from './forms/game-form/game-form.component';
-import { GameInfoFormComponent } from './forms/game-info-form/game-info-form.component';
-import { PostComponent } from './forms/post/post.component';
-import { ForumInfoFormComponent } from './forum/forum-info-form/forum-info-form.component';
-import { ManageForumsComponent } from './forum/manage-forums/manage-forums.component';
-import { ManageThreadsComponent } from './forum/manage-threads/manage-threads.component';
-import { ManageUsersComponent } from './forum/manage-users/manage-users.component';
-import { GameInfoComponent } from './game-info/game-info.component';
+// import { ConsoleFormComponent } from './forms/console-form/console-form.component';
+// import { ConsoleInfoFormComponent } from './forms/console-info-form/console-info-form.component';
+// import { ConsolePostComponent } from './forms/console-post/console-post.component';
+// import { DescriptorFormComponent } from './forms/descriptor-form/descriptor-form.component';
+// import { GameFormComponent } from './forms/game-form/game-form.component';
+// import { GameInfoFormComponent } from './forms/game-info-form/game-info-form.component';
+// import { PostComponent } from './forms/post/post.component';
+// import { ForumInfoFormComponent } from './forum/forum-info-form/forum-info-form.component';
+// import { ManageForumsComponent } from './forum/manage-forums/manage-forums.component';
+// import { ManageThreadsComponent } from './forum/manage-threads/manage-threads.component';
+// import { ManageUsersComponent } from './forum/manage-users/manage-users.component';
+// import { GameInfoComponent } from './game-info/game-info.component';
 import { GameComponent } from './game/game.component';
 
 
 const routes: Routes = [
 
-    { path: 'game/info/new', component: GameInfoFormComponent, canActivate: [AuthGuard, AdminAuthGuard] },
-    { path: 'forum/info/new', component: ForumInfoFormComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+    // { path: 'game/info/new', component: GameInfoFormComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+    // { path: 'forum/info/new', component: ForumInfoFormComponent, canActivate: [AuthGuard, AdminAuthGuard] },
 
-    { path: 'forum/threads', component: ManageThreadsComponent, canActivate: [AuthGuard, AdminAuthGuard] },
-    { path: 'forum/users', component: ManageUsersComponent, canActivate: [AuthGuard, AdminAuthGuard] },
-    { path: 'game/info', component: GameInfoComponent, canActivate: [AuthGuard, AdminAuthGuard] },
-    { path: 'game/new', component: GameFormComponent, canActivate: [AuthGuard, AdminAuthGuard] },
-    { path: 'game/:id', component: GameFormComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+    // { path: 'forum/threads', component: ManageThreadsComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+    // { path: 'forum/users', component: ManageUsersComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+    // { path: 'game/info', component: GameInfoComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+    // { path: 'game/new', component: GameFormComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+    // { path: 'game/:id', component: GameFormComponent, canActivate: [AuthGuard, AdminAuthGuard] },
 
-    { path: 'forum', component: ManageForumsComponent, canActivate: [AuthGuard, AdminAuthGuard] },
-    { path: 'game', component: GameComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+    // { path: 'forum', component: ManageForumsComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+    { path: 'game', component: GameComponent, canActivate: [AuthGuard, AdminA`uthGuard] },
 
     // admin home:
     // { path: '', component: '', canActivate: [AuthGuard, AdminAuthGuard] },
