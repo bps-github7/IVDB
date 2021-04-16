9906198312fc8c898f6b42909b42b1c76bf44660
Ben Sehnert <bps53@protonmail.com>
2021-04-16 13:04:58 -0400

    Forgot to add an important comment- that we need to be able to sort, filter and limit data provided in showContent (showNews, showStreams...) dropdowns.
    Worked on Content
    interface, mainly,
    by creating a new
    interface UserFeedback
    which is a part of content, but content
    does not extend it (could be thinking about this problem wrong,
    .. chew on this a bit)

--- src/app/models/content/Content.ts
+++ src/app/models/content/Content.ts
@@ -1,30 +1,31 @@
+import { UserFeedback } from "./user-feedback";
+
 export interface Content {
-    
-    /*
-    Original/unique data and materials
-    associated with this piece of content:
-    
 
-    description: short summary, a bio or abstract
-    body : the actual content being 
-    */
-    uid?: number,
-
-    title: string,
-    creator: string,
-    description?: string,
-    body?: string,
+  /*
+  Original/unique data and materials
+  associated with this piece of content:
 
 
-    // extra stuff
-    image?: (string | string []),
-    links?: (string | string []),
-    misc?: (string | string [])
+  description: short summary, a bio or abstract
+  body : the actual content being 
+  */
+  uid?: string,
 
-    // Metadata
-    created: number, // timestamp
-    category: string,
-    tags?: string [],
+  title: string,
+  creator: string,
+  description?: string,
+  body?: string,
 
 
+  // extra stuff
+  image?: (string | string []),
+  links?: (string | string []),
+  misc?: (string | string [])
+
+  // Metadata
+  created: number, // timestamp
+  category: string,
+  tags?: string [],
+  feedback?: UserFeedback
 }
