941454c3f439bc57ebceb29f8ff568ef879481d3
Ben Sehnert <bps53@protonmail.com>
2021-04-21 12:33:05 -0400

    Apps working, even after taking all the useless code in firebase.user subscription out. Im sure it introduces some breaking changes deep down in the app (likely: provide rating, provide review)
    just do a global search for references
    to localStorage, and replace them with
    ```
    userService.get(appUser.uid).subscribe(
    doc => {
      this.user = doc;
    }
    ```

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
-import { Component, Injectable, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { AuthService } from './core/auth.service';
-import { ProfileService } from './services/profile.service';
-import { UserService } from './services/user.service';
-// import * as firebase from 'firebase';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,40 +10,9 @@ import { UserService } from './services/user.service';
 
 export class AppComponent {
     title = 'ivdb';
-
-    //would be helpful if this could cast to User.
-    user: any;
-    displayName;
     
-    constructor(
-        private auth : AuthService,
-        private router : Router,
-        private userService : UserService,
-        // private route: ActivatedRoute,
-        // private profileService : ProfileService,         
-        ) {
-
-        //TODO: get rid of this whole jawn/ unneeded, but relied on throughout the app!...
-        this.auth.user$.subscribe(user => {
-            if (user) {
-                this.user = user;
-
-                //save the most recent user date to db
-                // this.userService.save(this.user);
-                
-
-                /* PLEASE use auth state and not local storage to keep userID and display name avialble throughout app!!! */
-                localStorage.setItem("user_id", this.user.uid);
-                localStorage.setItem("username", (this.user.displayName.includes(' ') ? this.user.displayName.split(' ').join('') : this.user.displayName));
-                
-                let returnUrl = localStorage.getItem('returnUrl');
-                this.router.navigateByUrl(returnUrl);
-            }
-        });
-    }
-
-    // This does not do what you want it to do!!
-    // ngOnInit() {
-    //     this.router.navigate([this.router.url])
-    // }   
+    constructor(private router : Router) {
+      let returnUrl = localStorage.getItem('returnUrl');
+      this.router.navigateByUrl(returnUrl);
+    }  
 }
