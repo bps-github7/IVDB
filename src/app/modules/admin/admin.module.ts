fb0e1325db48c32155248b852ef5ca7a631708b3
Ben Sehnert <bps53@protonmail.com>
2021-07-28 12:35:14 -0400

    Laying the groundwork for this improved UI for admin-threads. realized last night that we made a poor judgement, jumped into development, at multiple points, without much thought put into design. Falsely assumed that adminstering threads would follow suit from adminstering consoles, but the relation between games and consoles is very different from that of forums and threads, forums contain threads, games do not contain consoles... so we deleted the two components done yesterday- admin-forum-dropdown and thread-admin-view.
    Now before we start off work on this branch,
    we need to carefully define the page layout
    and component architecture -> AND IN THE FUTURE:
    do this before you start coding (duh) that way we
    can avoid this backtracking.

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -24,8 +24,6 @@ import { AdminDashboardTableManagerComponent } from './admin-dashboard/admin-das
 import { AdminDashboardFormComponent } from './admin-dashboard/admin-dashboard-form/admin-dashboard-form.component';
 import { AdminForumInfoComponent } from './admin-forum/admin-forum-info/admin-forum-info.component';
 import { AdminThreadsComponent } from './admin-forum/admin-threads/admin-threads.component';
-import { AdminForumDropdownComponent } from './admin-forum/admin-forum-dropdown/admin-forum-dropdown.component';
-import { ThreadAdminViewComponent } from './admin-forum/admin-forum-dropdown/thread-admin-view/thread-admin-view.component';
 
 @NgModule({
   declarations: [
@@ -46,8 +44,6 @@ import { ThreadAdminViewComponent } from './admin-forum/admin-forum-dropdown/thr
   AdminDashboardFormComponent,
   AdminForumInfoComponent,
   AdminThreadsComponent,
-  AdminForumDropdownComponent,
-  ThreadAdminViewComponent,
 	],
   imports: [
     CommonModule,
