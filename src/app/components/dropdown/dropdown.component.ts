444b3c38d71edd12efc4a99b22f2dc88891ec259
Ben Sehnert <bps53@protonmail.com>
2021-09-28 20:08:30 -0400

    Working on smart and dumb components for navbar. made a quick sketch of logic and markup for both, not done but its working in places.
    in process of adding styles, which in this implementation
    will simply add responsivness, not control whether
    or not a sub dropdown is shown.

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models';
 // import { AuthService } from '../../core/auth.service';
@@ -6,23 +6,41 @@ import { User } from 'src/app/models';
 // 
 
 
+interface Link {
+	url : string;
+	text : string
+}
+
 @Component({
   selector: 'dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.sass']
 })
 export class DropdownComponent implements OnInit {
-    appUser : User;
+	/**
+	 * dumb component for creating a single node (top button and hidden dropdown links)
+	 * for a nested dropdown employed in the site nav bar
+	 * 
+	 * 
+	 * @param {string | Link} - module : top level link for a module-
+	 * provides url and text for button at navbar top that,
+	 * when clicked, exposes the hidden dropdown menu. In
+	 * our case, this will be the root of feature module 
+	 * (ie Games, Content, User, Admin, Forum)
+	 * 	
+	 * @param {Link} [] - children : array of sublinks which
+	 * populate the menu of an unhidden dropdown. In our
+	 * case it will be the most often used links in a feature
+	 * module's component tree (ie user/profile, user/preferences...)
+	 * 
+	 */
 
-    // constructor(private auth : AuthService) { 
-    //     this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
-    // }
+	@Input() module : string | Link;
+	@Input() children : string | Link [];
 
+	hidden : boolean = true;
 
-		constructor() { }
+	constructor() { }
 
-    ngOnInit() { }
-
-    logout() { console.log("loging you out") }
-		// logout() { this.auth.logout(); }
+	ngOnInit() { }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -16,9 +16,68 @@ import firebase from 'firebase/app'
 })
 export class NavbarComponent implements OnInit {
 
+// still not super happy with this set up but it works for now
 	firebaseUser: firebase.User;
-
 	user$ : Observable<User>;
+
+	modules = [
+		"user",
+		"content",
+		"games",
+		"forum",
+		"admin"
+	]
+
+	// this is still redundant except in cases where the route is complex for example 'user/profile/edit'
+
+	// I suggest two configuration/ enhancements-
+	// 1) ability to pass another nested dropdown as a link object, which dropdown will know how to render recursively (flex ur angular muscles son!)
+	// 2) if the Link is an object, assume that its a complex link where {text : 'simple phrase', url : 'some/nested/route' }
+	// 	otherwise, the link is a string and we just need to prepend a slash for it to be a working link
+	children = {
+		// 'users' : [
+
+		// ],
+		'content' : [
+			'streams',
+			'news',
+			'watchlists',
+			'reviews',
+			'groups'
+		],
+		'admin' : [
+			'game',
+			'forum',
+			'user',
+			'content'
+		],
+		'games' : [],
+		'forum' : [],
+		'user' : []
+		// 'games' : [
+		// 	'browse',
+		// 	//this is an example of a potential nested link
+		// 	'game-info', 
+		// 	/*  { 'game-info' : [
+		// 		'categories',
+		// 		'creators',
+		// 		'console makers',
+		// 		'consoles'
+		// 	]}
+		// 	 */
+			
+		// 	// some potential ideas for complex link objects
+		// 	// 'rate',
+		// 	// 'top games'
+		// ],
+		// 'forum': [],
+		// 'users': [
+		// 	'browse',
+		// 	'connect'
+		// ],
+	} 
+
+
 
   constructor(
 		public auth : AuthService,
@@ -27,11 +86,9 @@ export class NavbarComponent implements OnInit {
 	async ngOnInit() {
 		this.firebaseUser = await firstValueFrom(this.auth.getUser$())
 		
+		/* ideally we'd be using the getUSerEntity$ method 
+		from auth service and not inject user store once again */
 		this.user$ = this.usersStore.pipe(select(selectUserById(this.firebaseUser.uid)))
-		// .subscribe(res => {
-		// 	console.log(res);
-		// 	this.user$ = res;
-		// })
 	}
 
 	logout() {
