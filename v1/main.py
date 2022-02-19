d7696c29dcb227ef59023a72409df06ad584dfeb
Ben Sehnert <bps53@protonmail.com>
2022-02-19 12:54:42 -0500

    Finally back on track with this. Fleshed out the main menu/ main module, which essentially is just a UI. Any actual command or database interfacing should be done in a different module and then facilitated by management facade, which is what accepts input at the bottom of the call stack in this module.

--- v1/main.py
+++ v1/main.py
@@ -1,5 +1,8 @@
-import sys
-from Game import Game
+"""Main module for videogame database application
+"""
+
+# from Game import Game
+# import sys
 
 def main():
     """Begins the program"""
@@ -7,38 +10,38 @@ def main():
     while loop:
         loop = main_menu()
 
+### This should go in the management facade.
+# def create_game():
+#     """Walks you through creating new game.
+#     Instantiate and return new game based on the gathered info
+#     """
+#     # obviously this will become more rigerous as time goes by
+#     # put these helper methods inside the class??
+#     return Game(input("provide game title: "), float(input("provide game price: ")), input("provide game description: "))
 
-def create_game():
-    """Walks you through creating new game.
-    Instantiate and return new game based on the gathered info
-    """
-    # obviously this will become more rigerous as time goes by
-    # put these helper methods inside the class??
-    return Game(input("provide game title: "), float(input("provide game price: ")), input("provide game description: "))
-
-def read_games(games : list) -> None:
-    # Think these menus should be a dictionary we enumerate over
-    for count,game in enumerate(games):
-        print(f"{count+1}. {game}")
-    while True:
-        print("type a number to select a game, or back to return to the previous menu")
-        choice = input()
-        # Catch all for bad inputs, might not work in 100% all cases
-        if choice not in ["b","back",] and int(choice) not in range(1, len(games)):
-            print("Sorry, your response was invalid")
-        if choice in ["b","back"]:
-            return False
-        if int(choice) in range(1, len(games)):
-            print("selected the game object")
-            print("what would you like to do with the game?")
-            print("1. edit")
-            print("2. delete")
-            print("3. rate")
-            choice_on_selected = int(input())
+# def read_games(games : list) -> None:
+#     # Think these menus should be a dictionary we enumerate over
+#     for count,game in enumerate(games):
+#         print(f"{count+1}. {game}")
+#     while True:
+#         print("type a number to select a game, or back to return to the previous menu")
+#         choice = input()
+#         # Catch all for bad inputs, might not work in 100% all cases
+#         if choice not in ["b","back",] and int(choice) not in range(1, len(games)):
+#             print("Sorry, your response was invalid")
+#         if choice in ["b","back"]:
+#             return False
+#         if int(choice) in range(1, len(games)):
+#             print("selected the game object")
+#             print("what would you like to do with the game?")
+#             print("1. edit")
+#             print("2. delete")
+#             print("3. rate")
+#             choice_on_selected = int(input())
 
 
 
-def main_menu(games):
+def main_menu():
     """facilitates C.R.U.D, Games is a list game objects modified in place and returned"""
     
     # you can do error catching for the admin interface here instead of inside sub fns
@@ -64,7 +67,7 @@ def prompt(admin=False):
 Presents the user with options
     """
     opt = ["1. Enter a videogame","2. Enter a rating for videogame",
-           "3. Display video games on record"]
+           "3. Display videogames on record"]
     if admin:
         opt.extend(["4. Edit a videogame", "5. Delete a videogame"])
     print("What would you lie to do?\
@@ -76,14 +79,11 @@ Presents the user with options
 def menu(choice, admin=False):
     """
 Return false to quit menu/prompt system,
-and True to continue looping, executes
-commands incommenserate with passed in argument
-which is the user's input
+and True to continue looping. otherwise,
+executes actions correspondent with
+the user's input (choice argument)
     """
     limit = 3 if admin else 5
-    if int(choice) in range(1,limit):
-        print("this is where the management facade goes to work with your choice")
-        print(f"you chose: {choice}")
     # if choice == "1":
     #     m.add(1)
     #     return True
@@ -100,15 +100,17 @@ which is the user's input
     #     m.display("Scholarship")
     #     print("\n")
     #     return True
-    elif choice.lower() in ("quit","q","done","d"):
+    if choice.lower() in ("quit","q","done","d"):
         return False
     elif choice.lower() in ("help","h"):
-        print("\n\n<---Help Page--->\nScholarship management Application:\n\nEnter a number to perform an action. \n\n\Make sure to only enter the number!\n\n\n")
+        print("\n\n<---Help Page--->\nVideo Game Database Application:\n\nEnter a number to perform an action. \n\n\Make sure to only enter the number!\n\n\n")
         return True
+    elif int(choice) in range(1,limit):
+        print("this is where the management facade goes to work with your choice")
+        print(f"you chose: {choice}")
     else:
         print("Error- ",choice," is not a valid option.\n\
 Please try again...")
         return True
-
 
 main()
